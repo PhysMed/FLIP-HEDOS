977f5f75e249469e54188272cba2bd3f023e4ca1
Marina García-Cardosa <mgarciacard@unav.es>
2026-04-02 13:35:58 +0200

    License comment added

--- workflows/ImportPatientFromMATLAB.py
+++ workflows/ImportPatientFromMATLAB.py
@@ -1,3 +1,6 @@
+# Under the GNU General Public License v3.0 (GPLv3):
+# Copyright (C) 2026 PhysMed Research Group - University of Navarra
+#
 # ========= Created by Marina Garcia-Cardosa in January 2024 ============
 import numpy as np
 from scipy.io import loadmat
