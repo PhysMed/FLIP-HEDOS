d5ccdf9d83da16905b058d9add619fa8146e24a6
Marina García-Cardosa <mgarciacard@unav.es>
2026-04-02 13:31:58 +0200

    License comment added

--- simulation/FlowModel.py
+++ simulation/FlowModel.py
@@ -1,4 +1,10 @@
-# ========= Created by Chris Beekman et al. 2023 ============
+# Under the GNU General Public License v3.0 (GPLv3):
+# Copyright (C) 2026 PhysMed Research Group - University of Navarra
+#
+# This file includes code licensed under the MIT License:
+# Copyright (c) 2021-2023 MGH Radiation Oncology
+#
+# ========= Updated by Chris Beekman et al. 2023 ============
 # ========= Modified by Marina Garcia-Cardosa since January 2024 to manage with data patients =========
 # ========= Last update done in Novemeber 2024 by Marina Garcia-Cardosa to enhance and optimize it =========
 import numpy as np
