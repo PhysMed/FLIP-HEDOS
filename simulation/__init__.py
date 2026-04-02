895ae1179b2e9a1c74f7f140fd429bc75773551b
Marina García-Cardosa <mgarciacard@unav.es>
2026-04-02 13:34:09 +0200

    License comment added

--- simulation/__init__.py
+++ simulation/__init__.py
@@ -1,4 +1,10 @@
-# ==== Created by Chris Beekman et al. 2023 ====
+# Under the GNU General Public License v3.0 (GPLv3):
+# Copyright (C) 2026 PhysMed Research Group - University of Navarra
+#
+# This file includes code licensed under the MIT License:
+# Copyright (c) 2021-2023 MGH Radiation Oncology
+#
+# ==== Updated by Chris Beekman et al. 2023 ====
 # ==== Modified by Marina Garcia-Cardosa to remove LoadPatient from simulation folder (in FLIP-HEDOS is not used) ====
 # -*- coding: utf-8 -*-
 from simulation.Weibull import Weibull
